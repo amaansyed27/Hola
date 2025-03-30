02ed171f0668b2b5b3d3a2c610605153256e0719
Amaan Syed <amaansyed27@gmail.com>
2025-03-31 00:25:24 +0530

    overlap fixed

--- src/pages/ViewGreetingPage.tsx
+++ src/pages/ViewGreetingPage.tsx
@@ -57,7 +57,8 @@ const ViewGreetingPage = () => {
   const effectsEnabled = greeting.continuousEffectEnabled !== false;
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-background relative px-4 py-12">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-background relative px-4 py-12 pb-24">
+      {/* Added 'pb-24' to provide extra padding at the bottom */}
       {/* Continuous effects container - only show if enabled by the sender */}
       {continuousEffect !== 'none' && effectsEnabled && (
         <ContinuousEffects
